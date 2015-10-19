e8b1df38999b8ca73fa46b8a30c21b96194c31b5
Kyle Derkacz <kderkacz@saymedia.com>
2015-10-19 15:46:21 -0700

    Incremental

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 'use strict';
 
-var module = angular.module('jsonSchemaDoc');
+var module = angular.module('jsonSchemaDoc', []);
 
 module.directive('jsonDoc', function () {
 
-
     return {
         restrict: 'EA',
+        template: '<div class="">' +
+            '    <h2 class="" ng-if="showTitle">{{schema.title}}</h2>' +
+            '    <span class="" ng-if="showDescription" ng-bind="schema.description"></span>' +
+            '    <table ng-if="schema.properties" class="">' +
+            '        <thead>' +
+            '            <tr>' +
+            '                <th>Property</th>' +
+            '                <th>Title</th>' +
+            '                <th>Description</th>' +
+            '                <th>Value</th>' +
+            '            </tr>' +
+            '        </thead>' +
+            '        <tbody>' +
+            '            <tr ng-repeat="(key, property) in schema.properties">' +
+            '                <td>{{key}}</td>' +
+            '                <td>{{property.title}}</td>' +
+            '                <td>{{property.description}}</td>' +
+            '                <td>{{property.type}}</td>' +
+            '            </tr>' +
+            '        </tbody>' +
+            '    </table>' +
+            '</div>',
+        scope: {
+            schema: '=',
+            options: '='
+        },
         link: function ($scope, $element, $attrs) {
-            
+            $scope.title = (schema.title && options.showTitle !== false);
+            $scope.showDescription = (schema.description && options.showDescription !== false);
         }
     };
 });
