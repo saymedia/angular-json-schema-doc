b98cb1d1a1e9f63b2fd270fee46897d8218fd024
Kyle Derkacz <kderkacz@saymedia.com>
2015-10-19 14:01:46 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var module = angular.module('jsonSchemaDoc');
+
+module.directive('jsonDoc', function () {
+
+
+    return {
+        restrict: 'EA',
+        link: function ($scope, $element, $attrs) {
+            
+        }
+    };
+});
